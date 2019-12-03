7453eb3131fba261580dcfbc15742c1be201e2af
Arthur543 <ayatsun@ukr.net>
2019-12-03 15:48:05 +0200

    fix text formatting issue

--- utils/create_message.py
+++ utils/create_message.py
@@ -6,6 +6,6 @@ def create_message(vacancies: List[Vacancy]) -> str:
     message = ''
 
     for vacancy in vacancies:
-        message += f' Название вакансии: {vacancy.title} \n Компания: {vacancy.company} \n Ссылка: {vacancy.link}\n\n'
+        message += f'*Название вакансии*: {vacancy.title} \n*Компания*: {vacancy.company} \n*Ссылка*: {vacancy.link}\n\n'
 
     return message
