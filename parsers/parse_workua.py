77cd551bcdf7e3c5c4bec9514fcb120bbb5721b7
Arthur543 <ayatsun@ukr.net>
2019-12-03 23:06:39 +0200

    complete work ua parser

--- parsers/parse_workua.py
+++ parsers/parse_workua.py
@@ -13,13 +13,18 @@ class WorkUaParser(Parser):
         soup = self.get_page_content(WORKUA_URL.format(self.search_query))
 
         cards = soup.findAll('div', {'class': 'card card-hover card-visited wordwrap job-link'})
+        hot_cards = soup.findAll('div', {'class': 'card card-hover card-visited wordwrap job-link js-hot-block'})
 
-        for card in cards:
+        for card in cards + hot_cards:
             title = card.find('h2').find('a').get_text()
             link = 'https://www.work.ua' + card.find('h2').find('a')['href']
             company = card.find('div', {'class': 'add-top-xs'}).find('span').get_text()
-            date = card.find('span', {'class': 'text-muted small'}).get_text()
-            date = self._transform_date(date)
+
+            try:
+                date = card.find('span', {'class': 'text-muted small'}).get_text()
+                date = self._transform_date(date)
+            except AttributeError:
+                date = datetime.date.today()
 
             vacancy = Vacancy(title=title, company=company, link=link, date=date)
             self.vacancies.append(vacancy)
@@ -28,18 +33,25 @@ class WorkUaParser(Parser):
 
     @staticmethod
     def _transform_date(date: str) -> datetime.date:
-        number, day_or_week = date.split('\xa0')[:2]
-        number = int(number)
-
         days_before = 0
-        if day_or_week.startswith('дня'):
-            days_before = number
-        elif day_or_week.startswith('нед'):
-            days_before = number * 7
+        hours_before = 0
 
-        return datetime.date.today() - datetime.timedelta(days=days_before)
+        if date.startswith('Вчера'):
+            days_before = 1
+        else:
+            number, day_or_week = date.split('\xa0')[:2]
+            number = int(number)
+
+            if day_or_week.startswith('дня'):
+                days_before = number
+            elif day_or_week.startswith('нед'):
+                days_before = number * 7
+            elif day_or_week.startswith('ч'):
+                hours_before = number
+
+        return datetime.date.today() - datetime.timedelta(days=days_before, hours=hours_before)
 
 
 if __name__ == '__main__':
-    vacancies = WorkUaParser('python').parse()
+    vacancies = WorkUaParser('Менеджер').parse()
     print(vacancies)
