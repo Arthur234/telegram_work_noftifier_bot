fa1f934b42db771fb56663db28a1d32339e2520e
Arthur543 <ayatsun@ukr.net>
2019-12-03 15:26:25 +0200

    figure out with heroku

--- bot.py
+++ bot.py
@@ -14,30 +14,25 @@ server = Flask(__name__)
 
 
 @bot.message_handler(commands=['start'])
-def main_menu(message):
-    bot.send_message(message.from_user.id, 'Выберите группу:')
+def send_welcome(message):
+    chat_id = message.chat.id
+    print(chat_id)
+
+    msg = bot.send_message(chat_id, 'Введите название вакансии')
+    bot.register_next_step_handler(msg, get_vacancy)
 
 
-# @bot.message_handler(commands=['start'])
-# def send_welcome(message):
-#     chat_id = message.chat.id
-#     print(chat_id)
-#
-#     msg = bot.send_message(chat_id, 'Введите название вакансии')
-#     bot.register_next_step_handler(msg, get_vacancy)
+def get_vacancy(message):
+    chat_id = message.chat.id
+    bot.send_message(chat_id, 'Ищем...')
 
+    vacancies = get_vacancies(message.text)
+    message = create_message(vacancies)
 
-# def get_vacancy(message):
-#     chat_id = message.chat.id
-#     bot.send_message(chat_id, 'Ищем...')
-#
-#     vacancies = get_vacancies(message.text)
-#     message = create_message(vacancies)
-#
-#     for msg in message.split('\n\n'):
-#         if msg:
-#             time.sleep(0.5)
-#             bot.send_message(chat_id, msg)
+    for msg in message.split('\n\n'):
+        if msg:
+            time.sleep(0.5)
+            bot.send_message(chat_id, msg)
 
 
 @server.route('/' + TOKEN, methods=['POST'])
