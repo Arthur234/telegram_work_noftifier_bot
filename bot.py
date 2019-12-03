064d65ea3807ffd4192aa6328aae9b4ebc7af761
Arthur543 <ayatsun@ukr.net>
2019-12-03 14:51:18 +0200

    change bot

--- bot.py
+++ bot.py
@@ -8,12 +8,15 @@ from flask import Flask, request
 from utils.get_vacancies import get_vacancies
 from utils.create_message import create_message
 
-bot = telebot.TeleBot(TOKEN)
+TOKEN = '996680656:AAEyecvkOAewCP4Qbmmyuxeg7Xruyeh9a1U'
+
+bot = telebot.TeleBot(token=TOKEN)
 server = Flask(__name__)
 
 
 @bot.message_handler(commands=['start'])
 def main_menu(message):
+    print(message)
     bot.send_message(message.from_user.id, 'Выберите группу:')
 
 
@@ -40,17 +43,17 @@ def main_menu(message):
 
 
 @server.route('/' + TOKEN, methods=['POST'])
-def getMessage():
+def get_message():
     bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
-    return "!", 200
+    return "POST", 200
 
 
 @server.route("/")
-def webhook():
+def web_hook():
     bot.remove_webhook()
-    bot.set_webhook(url='https://telegram-work-notifier-bot.herokuapp.com/' + TOKEN)
-    return "!", 200
+    bot.set_webhook(url='https://telegram-bot-s.herokuapp.com/' + TOKEN)
+    return "CONNECTED", 200
 
 
-if __name__ == "__main__":
-    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
+if __name__ == '__main__':
+    server.run(host="0.0.0.0", port=os.environ.get('PORT', 5000))
