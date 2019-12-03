fbd0ce043153c17d0e58a7582aaa92e667b27845
Arthur543 <ayatsun@ukr.net>
2019-12-03 15:40:30 +0200

    change djinni url

--- constants.py
+++ constants.py
@@ -1,4 +1,4 @@
 WORKUA_URL = 'https://www.work.ua/ru/jobs-dnipro-{}/'
 HH_URL = 'https://dnepr.hh.ua/search/vacancy?currency_code=UAH&text={}&area=2126'
-DJINNI_URL = 'https://djinni.co/jobs/?primary_keyword={}&location=Днепр'
+DJINNI_URL = 'https://djinni.co/jobs/?keywords={}&location=Днепр'
 RABOTAUA_URL = 'https://rabota.ua/zapros/{}/днепропетровск'
