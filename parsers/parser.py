4a65fdd8df16e3508a1058b34525c83a7aaab7a5
Arthur543 <ayatsun@ukr.net>
2019-12-04 14:43:07 +0200

    remove headers

--- parsers/parser.py
+++ parsers/parser.py
@@ -8,11 +8,10 @@ from bs4 import BeautifulSoup
 class Parser:
     def __init__(self, search_query):
         self.search_query = search_query
-        self.headers = {"User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:70.0) Gecko/20100101 Firefox/70.0"}
         self.vacancies = []
 
     def get_page_content(self, url: str):
-        page = requests.get(url, headers=self.headers)
+        page = requests.get(url)
         return BeautifulSoup(page.content, 'html.parser')
 
     def parse(self):
