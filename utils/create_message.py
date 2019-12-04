34b4963855a5575cb8290c6fdc09879c3468c34f
Arthur543 <ayatsun@ukr.net>
2019-12-04 18:03:20 +0200

    unsuccessed build

--- utils/create_message.py
+++ utils/create_message.py
@@ -6,7 +6,7 @@ def create_message(vacancies: List[Vacancy]) -> str:
     message = ''
 
     for vacancy in vacancies:
-        message += f'*{vacancy.date}* \n' \
+        message += f'*{vacancy.date}*\n' \
                    f'**Название вакансии**: {vacancy.title}\n' \
                    f'**Компания**: {vacancy.company}\n' \
                    f'_Ссылка_: {vacancy.link}\n\n'
